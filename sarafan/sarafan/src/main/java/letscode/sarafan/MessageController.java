8e45d21b2979f410bd17f0aaae1cf0964674cb09
andreybannikov <andrewnnov@yandex.ru>
2019-01-31 00:18:27 +0300

    test app

---
+++ sarafan/sarafan/src/main/java/letscode/sarafan/MessageController.java
@@ -0,0 +1,14 @@
+package letscode.sarafan;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("message")
+public class MessageController {
+    @GetMapping
+    public String list() {
+        
+    }
+}
